82048e45f34f51c03f172d76a80a464cc1b9d651
Abdiel Licona <abdiel.licona@ladylee.com>
2025-12-29 11:43:08 -0600

    Agregando prisma

---
+++ src/main.ts
@@ -0,0 +1,25 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api/v1');
+
+  // app.useGlobalPipes(
+  //   new ValidationPipe({
+  //     transform: true,
+  //     whitelist: true,
+  //     forbidNonWhitelisted: false, // Si se establece en true, en lugar de quitar las propiedades que no están en la lista blanca, el validador generará una excepción.
+  //   }),
+  // );
+
+  await app.listen(process.env.PORT ?? 3000);
+  if (process.env.NODE_ENV === 'dev') {
+    console.log(
+      `Application is running on: http://localhost:${process.env.PORT}`,
+    );
+  }
+}
+
+void bootstrap();
